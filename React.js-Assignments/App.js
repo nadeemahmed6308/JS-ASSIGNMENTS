6302cb7a0a149114c9a5d144b9b27b21ad7baac0
nadeemahmed6308 <145861674+nadeemahmed6308@users.noreply.github.com>
2024-01-17 07:42:54 +0500

    Add files via upload

--- React.js-Assignments/App.js
+++ React.js-Assignments/App.js
@@ -1,1 +1,17 @@
+import Home from './Pages/Home'
+import './App.css';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
+function App() {
+  return (
+    <div>
+    <Home />
+    <Login />
+    <Register />
+    </div>
+  );
+}
+
+export default App;
 
